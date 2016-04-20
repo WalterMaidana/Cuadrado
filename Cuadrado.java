410f9cbfe1c8bb50f4545aecd5b85249acf75ff5
WalterMaidana <walter.a.maidana@gmail.com>
2016-04-19 23:27:34 -0300

    AreaTriángulo

---
+++ Cuadrado.java
@@ -0,0 +1,10 @@
+package www.unlam.edu.ar;
+
+public class Cuadrado
+{
+	public Double areaCuadrado(Double baseCuadrado, Double alturaCuadrado)
+	  {
+		return baseCuadrado*alturaCuadrado;
+		  
+	  }
+}
